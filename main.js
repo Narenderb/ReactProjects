540f290f49f6ac7385f69b22a79ea1a5fce96b29
Narenderb <34243459+Narenderb@users.noreply.github.com>
2018-11-17 19:39:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import Expo from 'expo';
+import React from 'react';
+import Menu from './navigators/Menu';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Menu
+        onNavigationStateChange={() => ({ /* don't show navigation change logs */ })}
+      />
+    );
+  }
+}
+
+Expo.registerRootComponent(App);
